ee25d90b24979a9d25da65b1eaacc43777458322
Jaysen <77508949+programadorboricua@users.noreply.github.com>
2021-01-26 17:29:34 -0400

    Add files via upload
    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# Password Generator Project
+
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Bienvenidos al Generador de Contraseñas!")
+
+nr_letters = int(input("Cuantas letras quieres en tu Contraseña?\n"))
+nr_symbols = int(input(f"Cuantos simbolos quieres?\n"))
+nr_numbers = int(input(f"Cuantos numeros quieres?\n"))
+
+# crear lista vacia
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+    password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+    password_list += random.choice(numbers)
+
+# mezcla el password
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Tu Contraseña es: {password}")
